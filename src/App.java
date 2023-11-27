8401c27347a086a43dac470bf00270af4c63a119
TacodeAdobada <tacodeadobada21@gmail.com>
2023-11-27 07:48:08 -0800

    cambios finales

--- src/App.java
+++ src/App.java
@@ -1,12 +1,18 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 
 public class App extends JFrame{//CREAR EXTENDS PARA TRABAJAR CON SUBCLASES
+    //VENTANA
+    Dimension SZSC = Toolkit.getDefaultToolkit().getScreenSize();
 
-    int WIDTH = 800;    int HEIGHT = 1000;//SIZE VENTANA
+    int WIDTH = SZSC.width;    int HEIGHT = SZSC.height;//SIZE VENTANA
     int Desplazamiento = 35;//CENTRAR AREA DE JUEGO
+    int ColumnasG = 4;  int FilasG = 4;
     BufferedImage bi = new BufferedImage(this.WIDTH,this.HEIGHT,BufferedImage.TYPE_4BYTE_ABGR);//IMAGEN ESTANDAR
     Graphics gbi = bi.getGraphics();//GENERAR IMAGEN
 
@@ -18,16 +24,25 @@ public class App extends JFrame{//CREAR EXTENDS PARA TRABAJAR CON SUBCLASES
     }
 
     public void paint(Graphics g){//BUCLE
-        System.out.println("Test");
         this.pintarFondo(gbi);//PINTA EL FONDO ^
         g.drawImage(bi,10,10, this.WIDTH,this.HEIGHT,this);//LA IMAGEN COMPLETA ES GRAFICADA
     }
 
     public App(){
-        this.setSize(this.WIDTH,this.HEIGHT);//VENTANA SIZE
+        this.setSize(SZSC.width,SZSC.height);//VENTANA SIZE
+        //this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setVisible(true);//VENTANA
         this.setTitle("TIC TOC");//NOMBRE VENTANA
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        for(int i = 0;i<ColumnasG;i++){
+            for(int i2 = 0;i2<FilasG;i2++){
+                JButton Boton = new JButton();
+                Boton.setBounds(((this.WIDTH/3)*(i-1)),((this.HEIGHT/3)*(i2-1)),(this.WIDTH/3),(this.HEIGHT/3));
+                this.add(Boton);
+            }
+        }
     };//CONSTRUCTOR
     public static void main(String[] args) throws Exception {
         System.out.println("HOLA, COMENZANDO");
